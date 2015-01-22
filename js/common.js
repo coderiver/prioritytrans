ae166ccc67dffa10da5951f0b8f53d28f934a4f2
Vadim Hraboviy <hraboviyvadim@gmail.com>
2015-01-22 13:17:11 +0530

    feedback form, validation

--- js/common.js
+++ js/common.js
@@ -5,7 +5,6 @@ head.ready(function() {
 		$('.js-nav').toggleClass('is-open');
 	});
 
-
 	// slick news on main page
 
 	$('.js-slick').slick({
@@ -14,6 +13,40 @@ head.ready(function() {
 		dots: true,
 		arrows: false,
 		slidesToShow: 1
+	});
+
+	// validation
+
+	var form_validate = $('.js-validate');
+	if (form_validate.length) {
+		form_validate.each(function () {
+			var form_this = $(this);
+			$.validate({
+				form : form_this,
+				validateOnBlur : false,
+				borderColorOnError : false,
+				onSuccess : function() {
+      				$('.js-step1').addClass('is-hidden');
+      				$('.js-step2').removeClass('is-hidden');
+      				$('.js-form').addClass('is-success');
+      				$('.js-form-back').addClass('is-success');
+      				return false;
+    			}
+			});
+		});
+	};
+
+	// feedback form on main page
+
+	$('.js-form-btn').on('click', function(){
+		$(this).parents('.js-form').addClass('is-open');
+	});
+	$('.js-form-return').on('click', function(){
+		$('.js-validate').trigger('reset');
+		$('.js-form').removeClass('is-open is-success');
+      	$('.js-form-back').removeClass('is-success');
+      	$('.js-step1').removeClass('is-hidden');
+      	$('.js-step2').addClass('is-hidden');
 	});
 	
 });
